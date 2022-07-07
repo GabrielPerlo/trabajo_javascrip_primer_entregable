7750886e10d6748be188aadb446be01d0fc56b9a
Gabriel Perlo <Gabrielperloestelastalitas@gmail.com>
2022-07-06 22:15:04 -0300

    trabajo hasta el nuemro 4

---
+++ js/variables.js
@@ -0,0 +1,16 @@
+// mensaje de alerta
+alert("Un Mensaje");
+
+// mensaje html
+document.write("<h1>Hola Mundo</h1>");
+
+// resultado de suma
+document.write("Resultado:" + (5 + 3));
+
+// Nombre usuario
+let Nombre = prompt("Hola Nombre de Usuario")
+
+let num1 , num2 , total
+num1=prompt("digite el numero 1:")
+num2=prompt("digite el numero 2:")
+total=prompt("resultado")
